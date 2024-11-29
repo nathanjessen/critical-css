9e86f79b45147ae0c371c89f70203b1f159d5b8f
Nathan Jessen <nathan.jessen@aplaceformom.com>
2024-11-28 23:59:32 -0600

    update dependencies and gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -4,46 +4,43 @@ const cssFiles = ['index.css', 'lib/*.css'];
 
 const browserSync = require('browser-sync').create();
 const gulp = require('gulp');
-const cssnano = require('gulp-cssnano');
+const cssnano = require('cssnano');
 const notify = require('gulp-notify');
 const postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
 const size = require('gulp-size');
 const sourcemaps = require('gulp-sourcemaps');
-const immutableCss = require('immutable-css');
 const atVariables = require('postcss-at-rules-variables');
-const atIf = require('postcss-conditionals');
-const cssnext = require('postcss-cssnext');
-const cssEach = require('postcss-each');
-const atFor = require('postcss-for');
 const atImport = require('postcss-import');
 const reporter = require('postcss-reporter');
-const styleGuide = require('postcss-style-guide');
 const stylelint = require('stylelint');
+const postcssPresentEnv = require('postcss-preset-env');
+const postcssNesting = require('postcss-nesting');
+const postcssCustomProperties = require('postcss-custom-properties');
+const postcssEach = require('postcss-each');
 
 // CSS
 gulp.task('css', function () {
   var processors = [
     atImport(),
     stylelint(),
-    immutableCss(),
     reporter({
       clearReportedMessages: true,
       noIcon: true,
     }),
     atVariables(),
-    atFor(),
-    cssEach(),
-    atIf(),
-    cssnext({
+    postcssEach(),
+    postcssNesting(),
+    postcssCustomProperties(),
+    postcssPresentEnv({
       browsers: ['last 2 versions', '> 5%', 'not ie < 11'],
+      features: {
+        'nesting-rules': true,
+        'custom-properties': true,
+        'custom-media-queries': true,
+      },
     }),
-    styleGuide({
-      dest: siteRoot + '/index.html',
-      project: projectName,
-      showCode: true,
-      theme: 'default',
-    }),
+    cssnano(),
   ];
 
   return gulp
@@ -51,12 +48,11 @@ gulp.task('css', function () {
     .pipe(sourcemaps.init())
     .pipe(postcss(processors))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(siteRoot))
-    .pipe(cssnano())
     .pipe(size())
     .pipe(rename({ extname: '.min.css' }))
     .pipe(notify('css optimized'))
-    .pipe(gulp.dest(siteRoot));
+    .pipe(gulp.dest(siteRoot))
+    .pipe(browserSync.stream());
 });
 
 // BrowserSync
@@ -70,7 +66,7 @@ gulp.task('serve', () => {
   });
 
   // Watch
-  gulp.watch(cssFiles, gulp.parallel('css'));
+  gulp.watch(cssFiles, gulp.series('css'));
 });
 
 // Default
